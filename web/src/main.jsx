e97b1617ea0de40e808b63faa1002b453ba4e139
Zander <zander.aligato25@gmail.com>
2026-03-27 23:05:02 +0800

    feat(frontend): add GoogleOAuthProvider wrapper to app initialization

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId="186569633364-bq7fvo7pm9cspfpjog1uhc0t3nbc8bk2.apps.googleusercontent.com">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
