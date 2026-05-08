4076fd55292fe4a7a4623b3a15749bf5dcd5b717
Zander <zander.aligato25@gmail.com>
2026-05-09 00:36:48 +0800

    chore(legacy_files): cleanup legacy package structures and files

--- mobile/app/src/main/java/com/example/userregistrationandauthentication/data/AuthResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.userregistrationandauthentication.data
-
-data class AuthResponse(
-    val accessToken: String,
-    val refreshToken: String,
-    val user: UserResponse
-)
--- mobile/app/src/main/java/com/example/userregistrationandauthentication/data/LoginRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.example.userregistrationandauthentication.data
-
-data class LoginRequest(
-    val email: String,
-    val password: String
-)
