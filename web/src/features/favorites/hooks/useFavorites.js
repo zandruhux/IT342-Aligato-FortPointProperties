9d0aec45c9acc99c3ace3e80e709c4f5f13c6dec
Zander <zander.aligato25@gmail.com>
2026-05-22 18:03:46 +0800

    feat(favorites): add favorites feature barrel

--- web/src/features/favorites/hooks/useFavorites.js
+++ web/src/features/favorites/hooks/useFavorites.js
@@ -1,5 +1,5 @@
 import { useState, useCallback } from 'react';
-import * as favoritesApi from '../api/favoritesApi';
+import * as favoritesApi from '../api';
 import { useAuthContext } from '../../../shared/context/useAuthContext';
 
 /**
