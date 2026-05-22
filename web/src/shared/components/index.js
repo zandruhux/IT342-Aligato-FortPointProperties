85d38b888ac9ebc9479dbf87feeaf6c316d39665
Zander <zander.aligato25@gmail.com>
2026-05-22 18:03:25 +0800

    refactor(shared): add shared import barrels

---
+++ web/src/shared/components/index.js
@@ -0,0 +1,8 @@
+export { default as Button } from './ui/Button';
+export { default as LoadingSpinner } from './ui/LoadingSpinner';
+export { default as Modal } from './ui/Modal';
+export { default as HeroSection, HeroBackdrop } from './ui/HeroSection';
+export { default as Header } from './layout/Header';
+export { default as AdminSidebar } from './layout/AdminSidebar';
+export { default as AgentSidebar } from './layout/AgentSidebar';
+export { default as PropertySearchFilter } from './properties/PropertySearchFilter';
