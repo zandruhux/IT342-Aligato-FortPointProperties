738ec53c2ea7f0bfca42a1888831dff019ca427a
Zander <zander.aligato25@gmail.com>
2026-05-25 22:38:11 +0800

    chore(mobile-network): update local api and cleartext config

--- mobile/app/src/main/java/com/example/fortpointproperties/shared/network/ApiConfig.kt
+++ mobile/app/src/main/java/com/example/fortpointproperties/shared/network/ApiConfig.kt
@@ -2,7 +2,7 @@ package com.example.fortpointproperties.shared.network
 
 object ApiConfig {
     //CHANGED TO PC CONFIG
-    const val BASE_URL = "http://192.168.1.5:8080/"
+    const val BASE_URL = "http://192.168.1.7:8080/"
 
     // Shared paths for future mobile features.
     const val AUTH_BASE_PATH = "api/v1/auth/"
